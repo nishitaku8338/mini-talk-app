3d7fe17b623e9ac41d28d44fdca1057412650149
nishitaku8338 <nishitaku8338@gmail.com>
2021-05-05 15:28:44 +0900

    messages_controller.rbを編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
   def new
+    @messages = Mesaage.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(text: params[:message], [:text])
+    binding.pry
   end
 end
